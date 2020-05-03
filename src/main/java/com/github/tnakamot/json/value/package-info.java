7e604473ed99d514fdc397062e43fed9a66f310e
Takashi Nakamoto <nyakamoto@gmail.com>
2020-05-03 03:54:14 -0700

    Resolved one TODO.

--- src/main/java/com/github/tnakamot/json/value/package-info.java
+++ src/main/java/com/github/tnakamot/json/value/package-info.java
@@ -7,9 +7,8 @@
  * <p>
  * The key class is {@link com.github.tnakamot.json.value.JSONValue}. This
  * is a root class of other classes that represent various types of JSON
- * values.
- *
- * TODO: explain a bit more once the implementation is completed
+ * values. Actual representation of JSON values is achieved by subclasses of
+ * {@link com.github.tnakamot.json.value.JSONValue}.
  *
  * @see <a href="https://tools.ietf.org/html/rfc8259">RFC 8259</a>
  */
