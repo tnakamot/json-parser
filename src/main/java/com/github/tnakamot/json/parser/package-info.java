8f8258795d457eb89390241de21dab157e152d70
Takashi Nakamoto <nyakamoto@gmail.com>
2020-05-03 03:52:20 -0700

    Shortly documented JSONLexer and JSONParser.

--- src/main/java/com/github/tnakamot/json/parser/package-info.java
+++ src/main/java/com/github/tnakamot/json/parser/package-info.java
@@ -2,8 +2,11 @@
  * This package provides classes to parse JSON text based
  * on <a href="https://tools.ietf.org/html/rfc8259">RFC 8259</a>.
  *
- * <p>
- * TODO: explain a bit more about lexer and parser.
+ * {@link com.github.tnakamot.json.parser.JSONLexer} tokenizes a given
+ * {@link com.github.tnakamot.json.JSONText}.
+ * {@link com.github.tnakamot.json.parser.JSONParser} semantically
+ * analyzes the sequence of tokens and generate
+ * {@link com.github.tnakamot.json.value.JSONValue}.
  *
  * @see <a href="https://tools.ietf.org/html/rfc8259">RFC 8259</a>
  */
