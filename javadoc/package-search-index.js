8740ec72d1cafe4d04766ac2930b581354b809e4
Takashi Nakamoto <takashi.nakamoto@nao.ac.jp>
2020-05-09 16:28:21 -0700

    updated site

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.github.tnakamot.json"},{"l":"com.github.tnakamot.json.parser"},{"l":"com.github.tnakamot.json.token"},{"l":"com.github.tnakamot.json.value"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.github.tnakamot.json"},{"l":"com.github.tnakamot.json.parser"},{"l":"com.github.tnakamot.json.pointer"},{"l":"com.github.tnakamot.json.token"},{"l":"com.github.tnakamot.json.value"}]
