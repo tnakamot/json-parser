795e0dcfeb57bf84bc9c32ee977fdd09b0fa6608
Takashi Nakamoto <nyakamoto@gmail.com>
2020-05-10 15:08:49 -0700

    Updated URI information.

--- src/main/java/com/github/tnakamot/json/schema/package-info.java
+++ src/main/java/com/github/tnakamot/json/schema/package-info.java
@@ -1,6 +1,15 @@
 /**
  * This package provides classes for <a href="https://json-schema.org/specification.html">JSON
- * Schema</a> 2019-9.
+ * Schema</a> 2019-09.
+ *
+ * <p>
+ * Note that this library internally uses {@link java.net.URI} in the standard Java library to
+ * handel URIs. According to javadoc of Java 11, it conforms to
+ * <a href="http://www.ietf.org/rfc/rfc2396.txt">RFC 2396</a> and
+ * <a href="http://www.ietf.org/rfc/rfc2732.txt">RFC 2732</a>. However, the specification of
+ * JSON Schema 2019-09 refers to <a href="https://tools.ietf.org/html/rfc3986">RFC 3986</a>
+ * for the use of URIs. Therefore, this package is not fully compliant with the JSON Schema
+ * specification in this sense.
  *
  * @see <a href="https://json-schema.org/">JSON Schema</a>
  */
